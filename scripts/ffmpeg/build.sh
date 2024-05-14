47ed638b789b2df86b3f320cc737b2442f028f0b
Michael Zh <mikooomichael@gmail.com>
2024-05-14 14:47:47 -0400

    Disable unneeded FFmpeg features

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -48,6 +48,21 @@ EXTRA_LDFLAGS="-Wl,-z,max-page-size=16384 $DEP_LD_FLAGS"
   --enable-shared \
   --disable-static \
   --disable-vulkan \
+  --disable-encoders \
+  --disable-muxers \
+  --disable-protocols \
+  --enable-protocol=file \
+  --disable-filters  \
+  --disable-bsfs \
+  --disable-hwaccels \
+  --disable-devices \
+  --disable-debug \
+  --disable-decoders \
+  --disable-postproc \
+  --disable-network \
+  --disable-parsers \
+  --disable-demuxers \
+  --enable-demuxer=aac,flac,mp3,ogg,wav,avi,matroska,webm,mov,mp4,m4a \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
   $ADDITIONAL_COMPONENTS || exit 1
